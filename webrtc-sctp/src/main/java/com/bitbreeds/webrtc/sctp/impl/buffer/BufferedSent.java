431768fa3b6607ae976d5d9c597fab43843860fe
Jonas Waage <jonas.h.waage@gmail.com>
2018-02-25 21:55:13 +0100

    SendBuffer initial

---
+++ webrtc-sctp/src/main/java/com/bitbreeds/webrtc/sctp/impl/buffer/BufferedSent.java
@@ -0,0 +1,81 @@
+package com.bitbreeds.webrtc.sctp.impl.buffer;
+
+import com.bitbreeds.webrtc.sctp.impl.DataStorage;
+
+/**
+ * Copyright (c) 19/02/2018, Jonas Waage
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+public class BufferedReceived {
+
+    private final DataStorage data;
+    private final BufferedState bufferState;
+    private final DeliveredState deliverState;
+
+    public BufferedReceived(
+            DataStorage data,
+            BufferedState state,
+            DeliveredState deliverState) {
+        this.data = data;
+        this.bufferState = state;
+        this.deliverState = deliverState;
+    }
+
+    public DataStorage getData() {
+        return data;
+    }
+
+    public BufferedState getState() {
+        return bufferState;
+    }
+
+
+    public boolean canBeOverwritten() {
+        return BufferedState.FINISHED.equals(bufferState) && DeliveredState.DELIVERED.equals(deliverState);
+    }
+
+    public BufferedReceived acknowledge() {
+        //Control transition
+        return new BufferedReceived(data,BufferedState.ACKED,deliverState);
+    }
+
+    public BufferedReceived finish() {
+        return new BufferedReceived(data,BufferedState.FINISHED,deliverState);
+    }
+
+    public BufferedReceived deliver() {
+        return new BufferedReceived(data,bufferState,DeliveredState.DELIVERED);
+    }
+
+    public boolean readyForUnorderedDelivery() {
+        return DeliveredState.READY.equals(deliverState) && getData().getFlag().isUnordered();
+    }
+
+    public boolean readyForOrderedDelivery() {
+        return DeliveredState.READY.equals(deliverState) && getData().getFlag().isOrdered();
+    }
+
+    public boolean isDelivered() {
+        return DeliveredState.DELIVERED.equals(deliverState);
+    }
+
+    @Override
+    public String toString() {
+        return "BufferedReceived{" +
+                "data=" + data +
+                ", bufferState=" + bufferState +
+                ", deliverState=" + deliverState +
+                '}';
+    }
+}
---
+++ webrtc-sctp/src/main/java/com/bitbreeds/webrtc/sctp/impl/buffer/SendBuffer.java
@@ -0,0 +1,380 @@
+package com.bitbreeds.webrtc.sctp.impl.buffer;
+
+import com.bitbreeds.webrtc.common.SignalUtil;
+import com.bitbreeds.webrtc.sctp.impl.DataStorage;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/*
+ * Copyright (c) 19/02/2018, Jonas Waage
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/**
+ * Buffer to store SCTP messages for delivery defrag and creation of SACK
+ * <a href="https://tools.ietf.org/html/rfc4960#section-6.2.1">SCTP sack</a>
+ * <a href="https://tools.ietf.org/html/rfc2581#section-4.2">TCP congestion control</a>
+ *
+ * TODO Must handle TSN and Stream Sequence id rollover
+ *
+ */
+public class ReceiveBuffer {
+
+    private final Object lock = new Object();
+    private final Buffered[] buffer;
+    private int capacity;
+
+    private long cumulativeTSN;
+
+    private long maxReceivedTSN;
+
+    private long lowestDelivered;
+
+    private List<Long> duplicates;
+
+    private long receivedBytes = 0;
+    private long deliveredBytes = 0;
+    private boolean initialReceived = false;
+
+    private Map<Integer,Integer> orderedStreams = new HashMap<>();
+
+    public ReceiveBuffer(int bufferSize,int capacity) {
+        if(bufferSize <= 0) {
+            throw new IllegalArgumentException("Buffer must be above 0, is " + bufferSize);
+        }
+        if(capacity <= 0) {
+            throw new IllegalArgumentException("Capacity must be above 0, is " + bufferSize);
+        }
+        this.buffer = new Buffered[bufferSize];
+        this.capacity = capacity;
+        this.cumulativeTSN = -1;
+        this.maxReceivedTSN = -1;
+        this.lowestDelivered = -1;
+        this.duplicates = new ArrayList<>();
+    }
+
+    /**
+     *
+     * These getters are for monitoring, and can not be fully trusted.
+     */
+    public long getReceivedBytes() {
+        return receivedBytes;
+    }
+
+    public long getDeliveredBytes() {
+        return deliveredBytes;
+    }
+
+    public long getCumulativeTSN() {
+        return cumulativeTSN;
+    }
+
+    public long getCapacity() {
+        return capacity;
+    }
+
+    public void setInitialTSN(long initialTSN) {
+        synchronized (lock) {
+            this.cumulativeTSN = initialTSN;
+            this.maxReceivedTSN = initialTSN;
+            this.lowestDelivered = initialTSN;
+            this.initialReceived = true;
+        }
+    }
+
+    /**
+     * Store received message at TSN % size;
+     *
+     * @param data data to store
+     */
+    public void store(DataStorage data) {
+        int position = posFromTSN(data.getTSN());
+        synchronized (lock) {
+            if(!initialReceived) {
+                throw new InitialMessageNotReceived("Initial SCTP message not received yet, no initial TSN");
+            }
+            Buffered old = buffer[position];
+            if(old == null || old.canBeOverwritten()) {
+                buffer[position] = new Buffered(data,BufferedState.RECEIVED,DeliveredState.READY);
+                this.maxReceivedTSN = Math.max(this.maxReceivedTSN,data.getTSN());
+                this.capacity -= data.getPayload().length;
+                this.receivedBytes += data.getPayload().length;
+            }
+            else if(data.getTSN() == old.getData().getTSN()){
+                duplicates.add(data.getTSN());
+            }
+            else {
+                /*
+                 * Only malicious implementations should hit this unless we use a very small buffer
+                 */
+                List<Buffered> bad = Arrays.stream(buffer)
+                        .filter(i -> i != null && !i.canBeOverwritten())
+                        .collect(Collectors.toList());
+
+                throw new OutOfBufferSpaceError("Can not store since out of buffer space: "
+                + "Buffer " + bad
+                + "Capacity: " + this.capacity
+                + "TSN: " + this.cumulativeTSN);
+            }
+        }
+    }
+
+    /**
+     * @return sack data for creating complete SACK
+     */
+    public SackData getSackDataToSend() {
+        SackData data;
+        synchronized (lock) {
+            long newCumulativeTSN = findNewCumulativeTSN();
+            updateCumulativeTSN(newCumulativeTSN);
+            Set<Long> received = getReceived();
+            data = new SackData(newCumulativeTSN,received,duplicates,capacity);
+            duplicates = new ArrayList<>();
+        }
+        return data;
+    }
+
+
+    /**
+     *
+     * @return get messages for next layer
+     */
+    public List<Deliverable> getMessagesForDelivery() {
+        List<Deliverable> dl = new ArrayList<>();
+        synchronized (lock) {
+            int diff = (int)(maxReceivedTSN - lowestDelivered);
+            for (int i = 1; i<=diff ;i++) {
+                long tsn = lowestDelivered+i;
+                Buffered bf = getBuffered(tsn);
+                if (bf != null) {
+                    if (bf.readyForUnorderedDelivery()) {
+                        if (bf.getData().getFlag().isUnFragmented()) {
+                            dl.add(new Deliverable(bf.getData().getPayload(), 1));
+                            setBuffered(tsn, bf.deliver());
+                        } else {
+                            if (bf.getData().getFlag().isStart()) {
+                                finishFragment(bf)
+                                        .ifPresent(dl::add);
+                            }
+                        }
+                    } else if (bf.readyForOrderedDelivery()) {
+                        if (bf.getData().getFlag().isUnFragmented()) {
+                            receiveUnfragmentedBuffered(bf)
+                                    .ifPresent(deliverable -> {
+                                        dl.add(deliverable);
+                                        setBuffered(tsn, bf.deliver());
+                                    });
+                        } else {
+                            if (bf.getData().getFlag().isStart()) {
+                                if (nextInStream(bf.getData())) {
+                                    finishFragment(bf)
+                                            .ifPresent(dl::add);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            updateLowestDelivered(dl);
+            int sum = dl.stream()
+                    .map(i -> i.getData().length)
+                    .reduce(0, Integer::sum);
+
+            this.deliveredBytes += sum;
+            this.capacity += sum;
+        }
+        return dl;
+    }
+
+
+    /*
+     *
+     * @param ds
+     * @return
+     */
+    private boolean nextInStream(DataStorage ds) {
+        Integer sq = orderedStreams.get(ds.getStreamId());
+        return sq == null || sq+1 == ds.getStreamSequence();
+    }
+
+    /*
+     *
+     * @param buffered data
+     * @return
+     */
+    private Optional<Deliverable> receiveUnfragmentedBuffered(Buffered buffered) {
+        if(nextInStream(buffered.getData())) {
+            setBuffered(buffered.getData().getTSN(), buffered.deliver());
+            orderedStreams.put(buffered.getData().getStreamId(),buffered.getData().getStreamSequence());
+            return Optional.of(new Deliverable(buffered.getData().getPayload(),1));
+        }
+        return Optional.empty();
+    }
+
+    /**
+     *
+     * Update lowest deliverable, so we can use it for calc later
+     * @param deliverables current deliverables
+     */
+    private void updateLowestDelivered(List<Deliverable> deliverables) {
+        int fragments = deliverables.stream()
+                .map(Deliverable::getOriginalFragmentNumber)
+                .reduce(0,Integer::sum);
+
+        for (int i = 1; i <= fragments; i++) {
+            Buffered vf = getBuffered(lowestDelivered + i);
+            if (vf != null && vf.isDelivered()) {
+                lowestDelivered++;
+            } else {
+                break;
+            }
+        }
+    }
+
+    /**
+     * Retrieve position from TSN
+     * @param tsn to get position for
+     * @return position
+     */
+    private int posFromTSN(long tsn) {
+        return (int)(tsn % buffer.length);
+    }
+
+    /**
+     *
+     * @param tsn to get buffered data for
+     * @return buffered data for tsn, null if no data
+     */
+    private Buffered getBuffered(long tsn) {
+        return buffer[posFromTSN(tsn)];
+    }
+
+    /**
+     *
+     * @param tsn to set buffered data for
+     * @param buffered data
+     */
+    private void setBuffered(long tsn,Buffered buffered) {
+        buffer[posFromTSN(tsn)] = buffered;
+    }
+
+    /**
+     * Not thread safe, must happen in lock
+     *
+     * @return new cumulative tsn
+     */
+    private long findNewCumulativeTSN() {
+        long newCumulativeTSN = cumulativeTSN;
+        long diff = this.maxReceivedTSN - cumulativeTSN;
+        for (int i = 1; i <= diff; i++) {
+            Buffered bf = getBuffered(this.cumulativeTSN + i);
+            if (bf != null && !bf.canBeOverwritten()) {
+                newCumulativeTSN++;
+            } else {
+                break;
+            }
+        }
+        return newCumulativeTSN;
+    }
+
+    /**
+     * Not thread safe, must happen in lock
+     */
+    private void updateCumulativeTSN(long newCumulativeTSN) {
+        long diff = newCumulativeTSN - cumulativeTSN;
+        for (int i = 0; i < diff; i++) {
+            long tsn = this.cumulativeTSN + i;
+            Buffered bf = getBuffered(tsn);
+            if(bf != null) {
+                setBuffered(tsn, bf.finish());
+            }
+        }
+        this.cumulativeTSN = newCumulativeTSN;
+    }
+
+    /**
+     * Not thread safe, must happen in lock
+     */
+    private Set<Long> getReceived() {
+        Set<Long> data = new HashSet<>();
+        int diff = (int) (maxReceivedTSN - cumulativeTSN);
+        for (int i = 1; i <= diff; i++) {
+            long tsn = cumulativeTSN + i;
+            Buffered bf = getBuffered(tsn);
+            if (bf != null && !bf.canBeOverwritten()) {
+                data.add(bf.getData().getTSN());
+                setBuffered(tsn,bf.acknowledge());
+            }
+        }
+        return data;
+    }
+
+
+
+    private void setDelivered(List<Long> tsns) {
+        tsns.forEach(dlTsn -> {
+                    Buffered xs = getBuffered(dlTsn);
+                    setBuffered(dlTsn,xs.deliver());
+                }
+        );
+    }
+
+    private Deliverable fromTsns(List<Long> tsns) {
+        List<byte[]> data = tsns.stream()
+                .map(this::getBuffered)
+                .map(j->(j.getData()).getPayload())
+                .collect(Collectors.toList());
+        return new Deliverable(SignalUtil.joinBytesArrays(data),data.size());
+    }
+
+
+    /**
+     *
+     * @param start the start fragment
+     * @return deliverable defragmented message
+     */
+    private Optional<Deliverable> finishFragment(Buffered start) {
+        if(!start.getData().getFlag().isStart()) {
+            return Optional.empty();
+        }
+        else {
+            long tsn = start.getData().getTSN();
+            List<Long> good = new ArrayList<>();
+            good.add(tsn);
+
+            for (long i = tsn + 1; i <= maxReceivedTSN; i++) {
+                Buffered next = getBuffered(i);
+                if (next != null && !next.canBeOverwritten()) {
+                    DataStorage ds = next.getData();
+                    if(ds.getFlag().isMiddle()) {
+                        good.add(ds.getTSN());
+                    } else if(ds.getFlag().isEnd()) {
+                        good.add(ds.getTSN());
+                        Deliverable del = fromTsns(good);
+                        setDelivered(good);
+                        return Optional.of(del);
+                    }
+                } else {
+                    return Optional.empty();
+                }
+            }
+            return Optional.empty();
+        }
+    }
+
+
+
+
+}
