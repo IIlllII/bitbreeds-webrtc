dadef232bcbe8f03408801e86501dc84dd8980e5
Jonas Waage <jonas.h.waage@gmail.com>
2018-11-14 00:50:13 +0100

    Avoid killing connection setup

--- webrtc-signaling/src/test/java/com/bitbreeds/webrtc/signaling/LossyConnection.java
+++ webrtc-signaling/src/test/java/com/bitbreeds/webrtc/signaling/LossyConnection.java
@@ -3,11 +3,13 @@ package com.bitbreeds.webrtc.signaling;
 import com.bitbreeds.webrtc.peerconnection.ConnectionImplementation;
 import com.bitbreeds.webrtc.dtls.KeyStoreInfo;
 import com.bitbreeds.webrtc.peerconnection.PeerDescription;
+import com.bitbreeds.webrtc.sctp.model.SCTPMessage;
 import org.apache.commons.codec.binary.Hex;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /*
  * Copyright (c) 11/03/2018, Jonas Waage
@@ -40,6 +42,10 @@ public class LossyConnection extends ConnectionImplementation {
     private final Integer packetlossPercentageOut;
     private final Random random = new Random(System.currentTimeMillis());
 
+    private AtomicInteger outCount = new AtomicInteger(0);
+    private AtomicInteger inCount = new AtomicInteger(0);
+
+
     public LossyConnection(KeyStoreInfo keyStoreInfo,
                            PeerDescription remoteDescription,
                            Integer packetlossPercentageIn,
@@ -63,11 +69,15 @@ public class LossyConnection extends ConnectionImplementation {
      */
     @Override
     public void putDataOnWire(byte[] out) {
+        int cnt = outCount.incrementAndGet();
+
         int rd = random.nextInt(100);
-        if(rd > packetlossPercentageOut) {
+        if(rd > packetlossPercentageOut || cnt < 5) {
             super.putDataOnWire(out);
         }
         else {
+            SCTPMessage msg = SCTPMessage.fromBytes(out);
+            logger.info("Parsed: {}",msg);
             logger.info("Dropped out message with rd {} and data {}",rd, Hex.encodeHexString(out));
         }
     }
@@ -81,11 +91,15 @@ public class LossyConnection extends ConnectionImplementation {
      */
     @Override
     public void processReceivedMessage(byte[] buf) {
+        int cnt = inCount.incrementAndGet();
+
         int rd = random.nextInt(100);
-        if(rd > packetlossPercentageIn) {
+        if(rd > packetlossPercentageIn || cnt < 5) {
             super.processReceivedMessage(buf);
         }
         else {
+            SCTPMessage msg = SCTPMessage.fromBytes(buf);
+            logger.info("Parsed: {}",msg);
             logger.info("Dropped received message with rd {} and data {}",rd, Hex.encodeHexString(buf));
         }
     }
